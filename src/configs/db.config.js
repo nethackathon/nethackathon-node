4aa4faceead2be8c599115ee81bb10f016fdc18d
disperse <disperse.recording@gmail.com>
2023-08-14 11:05:02 -0400

    Updated db.config.js with the new server's socket location. TODO: move this to the .env file

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -3,7 +3,7 @@ const db = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
-  socketPath: '/tmp/mysql.sock',
+  socketPath: '/var/run/mysqld/mysqld.sock',
 };
 
 module.exports = db;
