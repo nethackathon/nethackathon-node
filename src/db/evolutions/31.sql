f43a6390595b930cbea593532d40485bafb5a1c3
disperse <disperse.recording@gmail.com>
2025-02-10 09:31:56 -0500

    Fixed up event_streamer -> event_schedule migrations.

--- src/db/evolutions/31.sql
+++ src/db/evolutions/31.sql
@@ -6,6 +6,7 @@ CREATE TABLE event_streamer (
     event_id BIGINT UNSIGNED NOT NULL,
     streamer_id BIGINT UNSIGNED NOT NULL,
     survey TEXT,
+    checklist TEXT,
     signed_up BOOLEAN DEFAULT false,
     schedule VARCHAR(4096) DEFAULT '{}',
     PRIMARY KEY (event_id, streamer_id),
@@ -15,20 +16,15 @@ CREATE TABLE event_streamer (
 );
 
 -- Copy unique pairs and set signed_up to true
-INSERT INTO event_streamer (event_id, streamer_id, survey, signed_up, schedule)
+INSERT INTO event_streamer (event_id, streamer_id, survey, signed_up)
 SELECT DISTINCT 
     event_id,
     streamer_id,
     MAX(survey),
-    true,
-    MAX(schedule)
+    true
 FROM event_schedule
 GROUP BY event_id, streamer_id;
 
--- Remove columns from event_schedule
+-- Remove column from event_schedule
 ALTER TABLE event_schedule 
-    DROP COLUMN signed_up,
-    DROP COLUMN schedule,
     DROP COLUMN survey;
-
-ALTER TABLE event_streamer ADD COLUMN checklist text DEFAULT null;
